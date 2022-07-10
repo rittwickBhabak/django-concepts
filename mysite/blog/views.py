249f140c35c7b63b3143422a3e305103409a5636
Rittwick Bhabak <rittwick2019@gmail.com>
2022-07-10 20:05:47 +0530

    taggit complete

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,16 +5,22 @@ from .models import Post
 class AllPost(ListView):
     model = Post 
 
+class TaggedPosts(ListView):
+    model = Post 
+    def get_queryset(self):
+        return Post.objects.filter(tags__name__in=[self.kwargs['tag']])
+    
+
 class ViewPost(DetailView):
     model = Post 
 
 class UpdatePost(UpdateView):
     model = Post 
-    fields = ["title", "content"]
+    fields = ["title", "content", "tags"]
 
 class CreatePost(CreateView):
     model = Post 
-    fields = ["title", "content"]
+    fields = ["title", "content", "tags"]
 
 class RemovePost(DeleteView):
     model = Post 
